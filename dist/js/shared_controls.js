b6ac13ce5b29ae68b6a1414c41a59f15c177ce0c
ViZarSmogon <vizarsmogon@gmail.com>
2025-01-10 20:22:40 +0100

    Update shared_controls.js

--- dist/js/shared_controls.js
+++ dist/js/shared_controls.js
@@ -289,7 +289,7 @@ $(".ability").bind("keyup change", function () {
 		$(this).closest(".poke-info").find(moveSelector).find(".move-hits").val(moveHits);
 	}
 
-	var TOGGLE_ABILITIES = ['Flash Fire', 'Intimidate', 'Minus', 'Plus', 'Slow Start', 'Unburden', 'Stakeout', 'Teraform Zero'];
+	var TOGGLE_ABILITIES = ['Flash Fire', 'Intimidate', 'Minus', 'Plus', 'Slow Start', 'Unburden', 'Stakeout', 'Teraform Zero', 'Sink or Swim'];
 
 	if (TOGGLE_ABILITIES.indexOf(ability) >= 0) {
 		$(this).closest(".poke-info").find(".abilityToggle").show();
